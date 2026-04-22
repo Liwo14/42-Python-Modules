749b57c68cb8c3913608a2c5db2c284a96aacca7
Liwo42 <celian.colnat@gmail.com>
2026-04-22 11:19:54 +0200

    blopblop

--- Module 0/ex7/ft_seed_inventory.py
+++ Module 0/ex7/ft_seed_inventory.py
@@ -6,12 +6,9 @@
 #  By: ccolnat <ccolnat@student.42.fr>           +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/04/22 09:32:48 by ccolnat         #+#    #+#               #
-#  Updated: 2026/04/22 11:06:53 by ccolnat         ###   ########.fr        #
+#  Updated: 2026/04/22 11:19:33 by ccolnat         ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
-
-import sys
-
 
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     if unit == "packets":
@@ -24,6 +21,3 @@ def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
                 print(f"{seed_type} seeds: covers {quantity} square meters")
             else:
                 print("Unknown unit type")
-
-
-ft_seed_inventory(sys.argv[1], int(sys.argv[2]), sys.argv[3])
